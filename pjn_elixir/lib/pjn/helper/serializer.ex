81e95d1d39859571c7134f4a6742ed95bf5cee91
mathek <front@student.agh.edu.pl>
2018-03-18 23:49:03 +0100

    improved judgments serialization

--- pjn_elixir/lib/pjn/helper/serializer.ex
+++ pjn_elixir/lib/pjn/helper/serializer.ex
@@ -5,7 +5,17 @@ defmodule Pjn.Helper.Serializer do
       |> Poison.encode!
       |> (&File.write path, &1).()
   end
-  
+
+  def dump_to_files(data, path, chunk_size, extension \\ ".json") do
+    data
+      |> Stream.chunk_every(chunk_size)
+      |> Stream.with_index
+      |> Flow.from_enumerable(max_demand: 1)
+      |> Flow.map(fn {data, i} -> {data |> Poison.encode!, i} end)
+      |> Flow.each(fn {data, i} -> File.write "#{path}#{i}#{extension}", data end)
+      |> Flow.run
+  end
+
   def read_from_file(path) do
     File.read!(path) |> Poison.decode!
   end
--- pjn_elixir/lib/pjn/judgments.ex
+++ pjn_elixir/lib/pjn/judgments.ex
@@ -1,19 +1,31 @@
 defmodule Pjn.Judgments do
 
-  def read(file_ids \\ 1..3173) do
-    base_path = "/Users/mathek/Desktop/shit/agh/pjn/data/json/"
-    pathes = file_ids
+  def read(dataset, file_ids \\ nil)
+
+  def read(:all, file_ids), do:
+    read_path(
+      "/Users/mathek/Desktop/shit/agh/pjn/data/json/",
+      file_ids || 1..3173,
+      fn %{"items" => items} -> items end
+    )
+
+  def read(2017, file_ids), do:
+    read_path(
+      "/Users/mathek/Desktop/shit/agh/pjn/data2017/json/",
+      file_ids || 0..8
+    )
+
+  def read_path(base_path, file_ids, unwrap_f \\ & &1) do
+    file_ids
       |> Stream.map(& base_path |> Path.join("judgments-#{&1}.json"))
-    pathes
       |> Flow.from_enumerable(max_demand: 1)
-      |> Flow.flat_map(fn path ->
-          %{"items" => items} = path
-            |> File.read!
-            |> Poison.decode!
-          items
-        end)
+      |> Flow.flat_map(& &1
+          |> File.read!
+          |> Poison.decode!
+          |> unwrap_f.()
+        )
   end
-  
+
   def filter_by_year(judgments, year) do
     year = "#{year}"
     judgments
@@ -21,4 +33,5 @@ defmodule Pjn.Judgments do
           y == year
         end)
   end
+
 end
